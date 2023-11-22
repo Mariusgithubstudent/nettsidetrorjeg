f16fc91cc0e94c4897a9dfb2be5c1ca460d8c650
mariusgithubstudent <marvaag@stud.ntnu.no>
2023-11-22 18:57:26 +0100

    min første commit. hvis det funker

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kul nettside!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Heisann hopsann!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hva skjer a, bagera?
         </p>
       </main>
 
-      <Footer />
     </div>
   )
 }
